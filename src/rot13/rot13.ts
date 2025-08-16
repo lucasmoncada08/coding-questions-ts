2dd6e161d6d0c461bfd8247e74b488637ce14dbd
Lucas Moncada <lucas.moncada08@gmail.com>
2025-08-16 16:26:23 -0400

    Initial commit

---
+++ src/rot13/rot13.ts
@@ -0,0 +1,52 @@
+
+const UPPER_UNICODE = 65;
+const LOWER_UNICODE = 97;
+const ALPHA_LENGTH = 26;
+const ROT13_SHIFT = 13;
+
+enum CharType {
+    Capitals = UPPER_UNICODE,
+    Lowercase = LOWER_UNICODE
+}
+
+export function rot13Shift (s: string): string {
+
+    let rot13String: string = "";
+    for (const character of s) {
+        let charCode: number = character.charCodeAt(0);
+
+        let rot13Num: number;
+        if (withinUpperOrLowerCaseRange(charCode, CharType.Capitals)) {
+            rot13Num = convertCharCodeToRot13(charCode, CharType.Capitals); 
+        }
+        else if (withinUpperOrLowerCaseRange(charCode, CharType.Lowercase)) {
+            rot13Num = convertCharCodeToRot13(charCode, CharType.Lowercase);  
+        }
+        else {
+            rot13Num = charCode; // Non-alphabetic characters remain unchanged
+        }
+        
+        rot13String += String.fromCharCode(rot13Num);
+    }
+
+    return rot13String;
+}
+
+function withinUpperOrLowerCaseRange(charCode: number, base: CharType): boolean {
+    if (base === CharType.Capitals) {
+        return charCode >= UPPER_UNICODE && charCode < UPPER_UNICODE + ALPHA_LENGTH;
+    } else if (base === CharType.Lowercase) {
+        return charCode >= LOWER_UNICODE && charCode < LOWER_UNICODE + ALPHA_LENGTH;
+    } else {
+        // This branch should never be hit since base is always CharType
+        throw new Error(`Invalid CharType: ${base}`);
+    }
+}
+
+function convertCharCodeToRot13(charCode: number, base: CharType): number {
+    let zeroBasedCharCode = charCode - base;
+    let shiftedCharCode = zeroBasedCharCode + ROT13_SHIFT;
+    let wrappedAround = shiftedCharCode % ALPHA_LENGTH;
+    let result = wrappedAround + base;
+    return result;
+};
