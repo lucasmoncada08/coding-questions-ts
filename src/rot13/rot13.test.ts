8cc75db932df9c0e6ca820cfea509fc31d761623
Lucas Moncada <lucas.moncada08@gmail.com>
2025-08-16 18:01:42 -0400

    adding linting and prettier to repository

--- src/rot13/rot13.test.ts
+++ src/rot13/rot13.test.ts
@@ -1,26 +1,25 @@
-import { rot13Shift } from './rot13';
-import { describe, test, expect } from 'vitest';
+import { rot13Shift } from "./rot13";
+import { describe, test, expect } from "vitest";
 
-describe('rot13Shift', () => {
-test('should convert char to ROT13 version', () => {
-  expect(rot13Shift('S')).toBe('F'); 
-  expect(rot13Shift('z')).toBe('m');
-  expect(rot13Shift('B')).toBe('O');
-  expect(rot13Shift('X')).toBe('K');
-  expect(rot13Shift('!')).toBe('!');
-  expect(rot13Shift(' ')).toBe(' ');
-  expect(rot13Shift('2')).toBe('2');
-});
+describe("rot13Shift", () => {
+  test("should convert char to ROT13 version", () => {
+    expect(rot13Shift("S")).toBe("F");
+    expect(rot13Shift("z")).toBe("m");
+    expect(rot13Shift("B")).toBe("O");
+    expect(rot13Shift("X")).toBe("K");
+    expect(rot13Shift("!")).toBe("!");
+    expect(rot13Shift(" ")).toBe(" ");
+    expect(rot13Shift("2")).toBe("2");
+  });
 
-test('should convert string to ROT13 version', () => {
-  expect(rot13Shift('abc')).toBe('nop');
-  expect(rot13Shift('gS')).toBe('tF');
-  expect(rot13Shift('FgS')).toBe('StF');
-  expect(rot13Shift('gS')).toBe('tF');
-  expect(rot13Shift('S')).toBe('F');
-  expect(rot13Shift('')).toBe('');
-  expect(rot13Shift('X78942 ue__/')).toBe('K78942 hr__/');
-});
-
+  test("should convert string to ROT13 version", () => {
+    expect(rot13Shift("abc")).toBe("nop");
+    expect(rot13Shift("gS")).toBe("tF");
+    expect(rot13Shift("FgS")).toBe("StF");
+    expect(rot13Shift("gS")).toBe("tF");
+    expect(rot13Shift("S")).toBe("F");
+    expect(rot13Shift("")).toBe("");
+    expect(rot13Shift("X78942 ue__/")).toBe("K78942 hr__/");
+  });
 });
 // "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
