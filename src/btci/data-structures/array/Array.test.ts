0d2c1878c7f324eb9154eddf9773925d52d94909
Lucas Moncada <lucas.moncada08@gmail.com>
2025-10-12 13:23:26 -0400

    feat: added capacity, pushing, of array creation + refactoring

--- src/btci/data-structures/array/Array.test.ts
+++ src/btci/data-structures/array/Array.test.ts
@@ -20,6 +20,24 @@ test("array starts empty", () => {
     assert.equal(arr.length, 0);
 })
 
+test("initialize the capacity of the array", () => {
+    const arr = new MyArray(8);
+    assert.equal(arr.capacity, 8);
+})
+
+test("push an element into the array", () => {
+    const arr = new MyArray();
+    arr.push(1);
+    assert.equal(arr.length, 1);
+})
+
+test("initialize array with preset values with of", () => {
+    const arr = MyArray.of(1, 2, 3);
+    assert.equal(arr.length, 3);
+})
+
+// get the value of a specific index
+
 if (anyFailure) {
     console.log("Errors Found!")
 } else {
--- src/btci/data-structures/array/Array.ts
+++ src/btci/data-structures/array/Array.ts
@@ -1,7 +1,31 @@
-export class MyArray<T> {
-  private readonly items: T[] = [];
+const DEFAULT_CAPACITY = 4;
 
-  get length(): number {
-    return this.items.length;
+export class MyArray<T> {
+  private data: Partial<Record<number, T>> = {};
+  private _capacity: number;
+  private _length: number = 0;
+
+  constructor(initialCapacity: number = DEFAULT_CAPACITY) {
+    this._capacity = initialCapacity;
+  }
+
+  push(item: T): void {
+    this.data[this._length++] = item;
+  }
+
+  static of<T>(...initialItems: T[]): MyArray<T> {
+    const arr = new MyArray<T>(Math.max(DEFAULT_CAPACITY, initialItems.length))
+    for (const item of initialItems) {
+      arr.push(item);
+    }
+    return arr;
+  }
+
+  get length(): number { 
+    return this._length;
+  }
+
+  get capacity(): number {
+    return this._capacity;
   }
 }
