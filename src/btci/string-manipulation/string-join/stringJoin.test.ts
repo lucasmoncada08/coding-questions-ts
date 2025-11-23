6a038e9a7f340a3bf39d5a1bbe28ff8d88b9f097
Lucas Moncada <lucas.moncada08@gmail.com>
2025-11-23 11:14:24 -0500

    feat: adding simple string joining

---
+++ src/btci/string-manipulation/string-join/stringJoin.test.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect } from 'vitest';
+import { joinString } from './stringJoin.ts';
+
+describe("test stringJoin", () => {
+    it("join a sentence together", () => {
+        const strings = ["join", "by", "space"];
+        expect(joinString(strings, " ")).toBe("join by space");
+    });
+
+    it("try with multiple char join", () => {
+        const strings = ['b', '', 'k', '', 'p', 'r n', '', 'd', 'd!!']
+        expect(joinString(strings, 'ee')).toBe('beeeekeeeepeer neeeedeed!!');
+    })
+})
